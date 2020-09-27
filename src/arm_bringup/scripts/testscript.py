dd754cad6fe3c26267a3b35f6f134b722fb115b3
dVeon-loch <blochdevon@gmail.com>
2020-09-27 20:49:25 +0200

    Added test script to command joint positions

---
+++ src/arm_bringup/scripts/testscript.py
@@ -0,0 +1,29 @@
+import rospy
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+def pos_commander(poslist):
+    rospy.init_node('joint_control')
+    pub = rospy.Publisher('/arm_controller/command', JointTrajectory, queue_size=10)
+
+    rate = rospy.Rate(10) # 10hz
+    pos_cmd = JointTrajectory()
+    pos_cmd.joint_names = ['plat_joint','shoulder_joint','forearm_joint','wrist_joint']
+
+    pt = JointTrajectoryPoint()
+
+    pt.positions = poslist
+
+
+    pt.time_from_start = rospy.Duration(2,0)
+
+    pos_cmd.points.append(pt)
+    rospy.sleep(1) # Needed so that the subscriber has enough time to subscribe to the publisher before publishing
+    pub.publish(pos_cmd)
+    rospy.spin()
+
+if __name__ == "__main__":
+    poslist = list(input("Enter Position \n"))
+    print(type(poslist))
+    poslist = [float(i) for i in poslist] 
+    print(poslist)
+    pos_commander(poslist)
